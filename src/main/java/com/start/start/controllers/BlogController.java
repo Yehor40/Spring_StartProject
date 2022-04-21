60a6a137f97c6037db2a32f52e6845e23df2a168
Yehor40 <fraugrey3@gmail.com>
2022-04-21 23:35:48 +0200

    some kind of shit was added and info from db is not showing.idk why((maybe bcs of fucking thymeleaf

--- src/main/java/com/start/start/controllers/BlogController.java
+++ src/main/java/com/start/start/controllers/BlogController.java
@@ -1,17 +1,27 @@
 package com.start.start.controllers;
 
+import com.start.start.models.Post;
+import com.start.start.repos.PostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
+
 public class BlogController {
 
+private PostRepo postRepo;
     @GetMapping("/blog")
-    public String main(Model model) {
-        model.addAttribute("title","Start project blog");
+    public String MainContrl(Model model) {
+        //kindda hello world
+        model.addAttribute("title","Start project text");
+        Iterable<Post> posts = postRepo.findAll();
+        model.addAttribute("posts",posts);
         return "blog";
+
     }
+
 
 }
