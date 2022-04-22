6424f4cdddb00380cdd8c5f65647e5f51e1846f4
Yehor40 <fraugrey3@gmail.com>
2022-04-22 13:13:02 +0200

    the main error was absence of @Autowired everything is showing problem fixed

--- src/main/java/com/start/start/controllers/BlogController.java
+++ src/main/java/com/start/start/controllers/BlogController.java
@@ -1,27 +1,34 @@
-package com.start.start.controllers;
+    package com.start.start.controllers;
+    import com.start.start.models.Post;
+    import com.start.start.repos.PostRepo;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
 
-import com.start.start.models.Post;
-import com.start.start.repos.PostRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+    import java.util.List;
 
 
-@Controller
+    @Controller
 
-public class BlogController {
+    public class BlogController {
+        @Autowired
+    private PostRepo postRepo;
 
-private PostRepo postRepo;
-    @GetMapping("/blog")
-    public String MainContrl(Model model) {
-        //kindda hello world
-        model.addAttribute("title","Start project text");
-        Iterable<Post> posts = postRepo.findAll();
-        model.addAttribute("posts",posts);
-        return "blog";
+        @GetMapping("/blog")
+        public String main(Model model) {
+            //kindda hello world
+            model.addAttribute("title","Start project text");
+            return "blog";
 
+        }
+
+        @GetMapping("/main")
+        public String Postmain(Model model) {
+            List<Post> posts = (List<Post>) postRepo.findAll();
+            model.addAttribute("posts",posts);
+
+            return "main";
+
+        }
     }
-
-
-}
